22263bb232ddfde0e4f732b108548b2554e7880a
Stian Lystad <Stianlystad_1@hotmail.com>
2022-08-10 18:35:27 +0000

    add models and admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Restaurant, Customer, Table, Reservation, Menu
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'city', 'country')
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+     list_display = ('email', 'first_name', 'last_name')
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('seats', 'min_people', 'max_people')
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_filter = ('approved', 'table')
+    list_display = ('party', 'table')
+    actions = ['approve_Reservation']
+
+    def approve_Reservation(self, request, queryset):
+        queryset.update(approved=True)
+
+@admin.register(Menu)
+class MenuAdmin(SummernoteModelAdmin):
+    list_display = ('nameOfFood', 'meat', 'allergy', 'food_details', 'price')
+
+    summernote_fields = ('food_details')
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=120)
+    address = models.CharField(max_length=120, default='Kings Place')
+    city = models.CharField(max_length=120, default='Kristiansund')
+    country = models.CharField(max_length=120, default='Norway')
+
+class Customer(models.Model):
+    email = models.EmailField()
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.first_name + " " + self.email
+
+class Table(models.Model):
+    seats = models.IntegerField()
+    min_people = models.IntegerField()
+    max_people = models.IntegerField()
+
+    def __str__(self):
+        return str(self.seats)
+
+
+class Reservation(models.Model):
+    table = models.ForeignKey('Table', on_delete=models.CASCADE)
+    party = models.ForeignKey('Customer', on_delete=models.CASCADE)
+    spot = models.DateField()
+    approved = models.BooleanField(default=False)
+
+class Menu(models.Model):
+    nameOfFood = models.CharField(max_length=30)
+    meat = models.CharField(max_length=30)
+    allergy = models.CharField(max_length=30)
+    food_details = models.CharField(max_length=150)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=149)
